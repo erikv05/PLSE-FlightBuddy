689842d111ea1bc64acf51599ef369cd3e1ec817
erikv05 <erikv05@uw.edu>
2024-07-02 23:34:32 -0400

    update app.js

--- client/src/App.js
+++ client/src/App.js
@@ -13,8 +13,6 @@ function App() {
   });
 
   const [showErrorBox, setShowErrorBox] = useState(false);
-  // const [showPredictionBox, setShowPredictionBox] = useState(false);
-
   const [prediction, setPrediction] = useState({
     showBox: false,
     data: "",
@@ -37,13 +35,6 @@ function App() {
       number: input.number,
       date: input.date,
     });
-    // setData({
-    //   // code: evt.target.value,
-    //   // number: data.number,
-    //   // date: data.date,
-    //   error_msg: data.error_msg,
-    //   prediction: data.prediction,
-    // });
   }
 
   function doFlightChange(evt) {
@@ -52,13 +43,6 @@ function App() {
       number: evt.target.value,
       date: input.date,
     });
-    // setData({
-    //   // code: data.code,
-    //   // number: evt.target.value,
-    //   // date: data.date,
-    //   error_msg: data.error_msg,
-    //   prediction: data.prediction,
-    // });
   }
 
   function doDateChange(evt) {
@@ -67,13 +51,6 @@ function App() {
       number: input.number,
       date: evt.target.value,
     });
-    // setData({
-    //   // code: data.code,
-    //   // number: data.number,
-    //   // date: evt.target.value,
-    //   error_msg: data.error_msg,
-    //   prediction: data.prediction,
-    // });
   }
 
   function doSubmitResp(res) {
@@ -90,18 +67,12 @@ function App() {
   function doSubmitJson(json) {
     if (json["error_msg"] !== undefined) {
       setData({
-        // code: data.code,
-        // number: data.number,
-        // date: data.date,
         prediction: "",
         error_msg: json["error_msg"],
       });
       setShowErrorBox(true);
     } else {
       setData({
-        // code: data.code,
-        // number: data.number,
-        // date: data.date,
         prediction: json["prediction"],
         error_msg: "",
       });
@@ -112,17 +83,11 @@ function App() {
   function doSubmitError(msg) {
     if (typeof msg !== "string") {
       setData({
-        // code: data.code,
-        // number: data.number,
-        // date: data.date,
         error_msg: "Couldn't fetch from server",
         prediction: "",
       });
     }
     setData({
-      // code: data.code,
-      // number: data.number,
-      // date: data.date,
       error_msg: msg,
       prediction: "",
     });
@@ -291,8 +256,6 @@ function App() {
           </div>
         </div>
         <br></br>
-        {/* <p className=" text-gray-400">Enter flight number: </p> */}
-        {/* break */}
         <div className="w-80">
           <div className="relative w-full min-w-[200px] h-10">
             <input
@@ -306,7 +269,6 @@ function App() {
               Flight Number
             </label>
           </div>
-          {/* break */}
         </div>
         <br></br>
         <div className="relative w-full min-w-[200px] h-10">
@@ -318,7 +280,7 @@ function App() {
             value={input.date}
           />
           <label className="flex w-full h-full select-none pointer-events-none absolute left-0 font-normal !overflow-visible truncate peer-placeholder-shown:text-blue-gray-500 leading-tight peer-focus:leading-tight peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500 transition-all -top-1.5 peer-placeholder-shown:text-sm text-[11px] peer-focus:text-[11px] before:content[' '] before:block before:box-border before:w-2.5 before:h-1.5 before:mt-[6.5px] before:mr-1 peer-placeholder-shown:before:border-transparent before:rounded-tl-md before:border-t peer-focus:before:border-t-2 before:border-l peer-focus:before:border-l-2 before:pointer-events-none before:transition-all peer-disabled:before:border-transparent after:content[' '] after:block after:flex-grow after:box-border after:w-2.5 after:h-1.5 after:mt-[6.5px] after:ml-1 peer-placeholder-shown:after:border-transparent after:rounded-tr-md after:border-t peer-focus:after:border-t-2 after:border-r peer-focus:after:border-r-2 after:pointer-events-none after:transition-all peer-disabled:after:border-transparent peer-placeholder-shown:leading-[3.75] text-gray-500 peer-focus:text-gray-900 before:border-blue-gray-200 peer-focus:before:!border-gray-900 after:border-blue-gray-200 peer-focus:after:!border-gray-900">
-            Flight Date (yyyy-mm-dd)
+            Flight Date (YYYY-MM-DD)
           </label>
         </div>
         <br></br>
@@ -329,20 +291,6 @@ function App() {
           value="Submit"
         >
           Submit
-          {/* <svg
-            class="w-4 h-4 ml-2"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M14 5l7 7m0 0l-7 7m7-7H3"
-            ></path>
-          </svg> */}
         </button>
       </form>
       {prediction.showBox ? (
