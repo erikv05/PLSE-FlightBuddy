1e4cb458794b89eb4366d21b76a9b13b472ef0c3
erikv05 <erikv05@uw.edu>
2024-07-05 12:16:56 -0400

    Clean up requirements.txt

--- server/app.py
+++ server/app.py
@@ -1,6 +1,5 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
-import hashlib
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
