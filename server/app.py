0070aa4674fcf16cf0dcaf47e89ec794b388a661
erikv05 <erikv05@uw.edu>
2024-07-05 12:23:03 -0400

    Update app.py to use if instead of match

--- server/app.py
+++ server/app.py
@@ -12,19 +12,17 @@ def predict():
     else:
         try:
             hashNum: int = hash(request.args.get('number')) % 10
-            print(hashNum)
             prediction: int
-            match hashNum:
-                case hashNum if 0 <= hashNum <= 3:
-                    prediction = 0
-                case hashNum if 4 <= hashNum <= 5:
-                    prediction = 1
-                case hashNum if 6 <= hashNum <= 7:
-                    prediction = 2
-                case 8:
-                    prediction = 3
-                case 9:
-                    prediction = 4
+            if 0 <= hashNum <= 3:
+                prediction = 0
+            elif 4 <= hashNum <= 5:
+                prediction = 1
+            elif 6 <= hashNum <= 7:
+                prediction = 2
+            elif hashNum == 8:
+                prediction = 3
+            else:
+                prediction = 4
             
             return jsonify({"prediction": prediction}), 200
         except Exception as e:
